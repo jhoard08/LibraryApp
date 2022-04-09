275dfe5f14134bb5dfb181b81b0d3c8d8cd85a25
Jacob Hoard <jacob37857@gmail.com>
2022-04-09 16:46:51 -0400

    refactor to classes

--- scripts/index.js
+++ scripts/index.js
@@ -3,35 +3,37 @@ const display = document.querySelector('.display');
 
 let libraryBooks = [];
 
-function Book(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function addBookToLibrary(name, author, pages, read) {
-    let book = new Book(name, author, pages, read);
-    return libraryBooks.push(book);
-}
-
-function displayLibrary(book) {
-    document.getElementById('bookList').innerHTML = book.map(book =>
-        `<tr>
-            <td>${book.name}</td>
-            <td>${book.author}</td>
-            <td>${book.pages}</td>
-            <td>${book.read}</td>
-            <td><button type="button" class="btn btn-danger delete">X</button></td>
-        </tr>
-        `).join('');
-}
-
-// Delete element from the DOM
-function deleteBook(target) {
-    if (target.classList.contains('delete')) {
-        target.parentElement.parentElement.remove();
+class Book {
+    constructor(){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
+
+    addBookToLibrary(name, author, pages, read) {
+        let book = new Book(name, author, pages, read);
+        return libraryBooks.push(book);
+    }
+
+    deleteBook(target) {
+        if (target.classList.contains('delete')) {
+            target.parentElement.parentElement.remove();
+        }
+    }
+
+    displayLibrary(book) {
+        document.getElementById('bookList').innerHTML = book.map(book =>
+            `<tr>
+                <td>${book.name}</td>
+                <td>${book.author}</td>
+                <td>${book.pages}</td>
+                <td>${book.read}</td>
+                <td><button type="button" class="btn btn-danger delete">X</button></td>
+            </tr>
+            `).join('');
+    }
+   
 }
 
 // Event: Remove book
@@ -56,11 +58,12 @@ create.addEventListener('click', () => {
     }
     // checks to see if any of the data is an empty string 
     if (book !== "" && author !== "" && pages !== "") {
-        addBookToLibrary(book, author, pages, yesOrNo);
+        let newBook = new Book();
+        newBook.addBookToLibrary(book, author, pages, yesOrNo);
 
         //resets form to clear 
         document.forms['form-container'].reset();
-        return displayLibrary(libraryBooks);
+        return newBook.displayLibrary(libraryBooks);
         //console.log(libraryBooks);
     }
     return alert('Please fill out all the information!')
